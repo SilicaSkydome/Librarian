529e88a35897d334ae415c6dc3db72cb81d03246
SilicaSkydome <77743412+SilicaSkydome@users.noreply.github.com>
2022-04-03 16:02:28 +0300

    Add files via upload

---
+++ forms/Orders.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Librarian.forms
+{
+    public partial class Orders : Form
+    {
+        //Подключение БД
+        SqlConnection sqlConnection = null;
+        SqlDataAdapter dataAdapter = null;
+        DataSet dataSet = null;
+
+        int bookID;
+
+        public Orders()
+        {
+            InitializeComponent();
+        }
+        private void Orders_Load(object sender, EventArgs e)
+        {
+            orderName.Text = User.Name;
+            orderLogin.Text = User.Login;
+
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["librarianDatabase"].ConnectionString);
+            sqlConnection.Open();
+
+            dataAdapter = new SqlDataAdapter(
+                "SELECT TOP 1000 Id, Name, Author, Publisher, Type, Genre, Year FROM Book",
+                sqlConnection);
+            dataSet = new DataSet();
+            dataAdapter.Fill(dataSet);
+            orderView.DataSource = dataSet.Tables[0];
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            if (orderSearch.Text == "")
+            {
+                dataAdapter = new SqlDataAdapter(
+                "SELECT TOP 1000 Id, Name, Author, Publisher, Type, Genre, Year FROM Book",
+                sqlConnection);
+                dataSet = new DataSet();
+                dataAdapter.Fill(dataSet);
+                orderView.DataSource = dataSet.Tables[0];
+            }
+            else if (searchAuthor.Checked && orderSearch.Text != "")
+            {
+                dataAdapter = new SqlDataAdapter(
+                $"SELECT TOP 50 Id, Name, Author, Publisher, Type, Genre, Year FROM Book where Author LIKE N'%{orderSearch.Text}%'",
+                sqlConnection);
+                dataSet = new DataSet();
+                dataAdapter.Fill(dataSet);
+                orderView.DataSource = dataSet.Tables[0];
+            }
+            else if (searchName.Checked && orderSearch.Text != "")
+            {
+                dataAdapter = new SqlDataAdapter(
+                $"SELECT TOP 50 Id, Name, Author, Publisher, Type, Genre, Year FROM Book where Name LIKE N'%{orderSearch.Text}%'",
+                sqlConnection);
+                dataSet = new DataSet();
+                dataAdapter.Fill(dataSet);
+                orderView.DataSource = dataSet.Tables[0];
+            }
+            else if (searchGenre.Checked && orderSearch.Text != "")
+            {
+                dataAdapter = new SqlDataAdapter(
+                $"SELECT TOP 50 Id, Name, Author, Publisher, Type, Genre, Year FROM Book where Genre LIKE N'%{orderSearch.Text}%'",
+                sqlConnection);
+                dataSet = new DataSet();
+                dataAdapter.Fill(dataSet);
+                orderView.DataSource = dataSet.Tables[0];
+            }
+            else if (searchYear.Checked && orderSearch.Text != "")
+            {
+                dataAdapter = new SqlDataAdapter(
+                $"SELECT TOP 50 Id, Name, Author, Publisher, Type, Genre, Year FROM Book where Year = {orderSearch.Text}",
+                sqlConnection);
+                dataSet = new DataSet();
+                dataAdapter.Fill(dataSet);
+                orderView.DataSource = dataSet.Tables[0];
+            }
+        }
+
+        private void orderView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            bookID = Convert.ToInt32(orderView.Rows[e.RowIndex].Cells[0].Value);
+        }
+
+        private void orderCreate_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand($"INSERT INTO Orders values ({bookID}, {User.ID}, GETDATE());", 
+                sqlConnection);
+
+            command.ExecuteNonQuery();
+        }
+
+
+    }
+}
---
+++ forms/Profile.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace Librarian.forms
+{
+    public partial class Profile : Form
+    {
+        //Подключение БД
+        SqlConnection sqlConnection = null;
+        SqlDataAdapter dataAdapter = null;
+        DataSet dataSet = null;
+        public Profile()
+        {
+            InitializeComponent();
+        }
+
+        private void Profile_Change_MouseDown(object sender, MouseEventArgs e)
+        {
+            Profile_Change.BackColor = Color.FromArgb(224, 158, 58);
+        }
+
+        private void Profile_Change_MouseUp(object sender, MouseEventArgs e)
+        {
+            Profile_Change.BackColor = Color.FromArgb(152, 22, 22);
+        }
+
+        private void Profile_Change_MouseEnter(object sender, EventArgs e)
+        {
+            Profile_Change.BackColor = Color.FromArgb(152, 22, 22);
+        }
+
+        private void Profile_Change_MouseLeave(object sender, EventArgs e)
+        {
+            Profile_Change.BackColor = Color.FromArgb(224, 58, 58);
+        }
+
+        private void Profile_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["librarianDatabase"].ConnectionString);
+            sqlConnection.Open();
+
+            textName.Text = User.Name;
+            textLogin.Text = User.Login;
+            textPassword.Text = User.Password;
+            textStatus.Text = User.Status;
+
+            dataAdapter = new SqlDataAdapter(
+                $"select ID, (select [Name] from Book where ID = Orders.BookID) as 'Book Name', DateGiven from Orders where UserID = {User.ID};",
+                sqlConnection);
+            dataSet = new DataSet();
+            dataAdapter.Fill(dataSet);
+            profileHistory.DataSource = dataSet.Tables[0];
+        }
+
+        private void Profile_Change_Click(object sender, EventArgs e)
+        {
+            SqlCommand sqlCommand = new SqlCommand($"UPDATE [User] SET Name = N'{textName.Text}', Login = N'{textLogin.Text}', Password = N'{textPassword.Text}', Status = N'{textStatus.Text}' where ID = {User.ID}", 
+                sqlConnection);
+
+            sqlCommand.ExecuteNonQuery();
+
+            MessageBox.Show("Данные изменены.", "Профиль", MessageBoxButtons.OK);
+        }
+    }
+}
